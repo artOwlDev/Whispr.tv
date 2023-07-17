6a8d42dbb64b93c4168123fdf265cc34995f883c
Artun <owlartpuni@gmail.com>
2023-07-17 15:29:08 -0300

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,9 +11,6 @@ const Footer = () => {
         <div className="footer-links">
           <SocialIcon  url='https://github.com/artOwlDev' fgColor="white" target="_blank" />
           <SocialIcon url='https://www.linkedin.com/in/artunselcuk/' fgColor="white" target="_blank" />
-          <SocialIcon url='https://www.youtube.com' fgColor="white" target="_blank" />
-          <SocialIcon url='https://twitter.com' fgColor="white" target="_blank" />
-          <SocialIcon url='https://www.instagram.com/artunipunii/' fgColor="white" target="_blank" />
         </div>
 
         <p>Whispr © 2023 | Data Collected from TMDB API</p>
